50eee9a21d48f5dd721832eabdf1aa60dd66e795
Felipe C. do Rego Pinto <felipecrp@gmail.com>
2016-12-07 16:15:45 -0200

    watchfy

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,87 @@ var cleanCss = require("gulp-clean-css");
 var rename = require("gulp-rename");
 var browserSync = require("browser-sync").create();
 var webpack = require("webpack-stream");
-
+var browserify = require("browserify");
+var watchify = require("watchify");
+var tsify = require("tsify");
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
 var tsc = ts.createProject("tsconfig.json");
+var fs = require("fs");
+
+/**
+ * load dependencies from package.json
+ */
+function loadDependencies() {
+    let packageJson = JSON.parse(fs.readFile("./package.json"));
+    let dependencies = [];
+
+    Object.keys(packageJson.dependencies).forEach(function(dependency) {
+        dependencies.push(dependency);
+    });
+
+    return dependencies;
+}
 
 /**
  * Generate javascript
  */
-gulp.task("js", function() {
-    return gulp.src("app/main.ts")
-            .pipe(webpack(require("./webpack.config.js")))
-            //.pipe(sourcemaps.init({loadMaps: true}))
-            //.pipe(uglify())
-            //.pipe(sourcemaps.write())
-            //.pipe(rename("keepitsafe.js"))
+var js = watchify(browserify({
+    debug: true,
+    entries: ['app/main.ts'],
+    cache: {}, 
+    packageCache: {},
+    plugin: [tsify]
+}));
+
+function bundleJs() {
+    return js.bundle()
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(rename("keepitsafe.js"))
+            .pipe(gulp.dest("build/app"))
+            .pipe(browserSync.stream());
+}
+gulp.task("js:watchify", function() {
+    return bundleJs();
+});
+
+gulp.task("js:browserify", function() {
+    return browserify({
+                debug: true,
+                entries: ['app/main.ts'],
+                cache: {}, 
+                packageCache: {} 
+            })
+            .plugin([tsify])
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(rename("keepitsafe.js"))
             .pipe(gulp.dest("build/app"))
             .pipe(browserSync.stream());
 });
+
+var myConfig = Object.create(require("./webpack.config.js"));
+gulp.task("js:webpack", function() {
+    return gulp.src("app/main.ts")
+            .pipe(webpack(myConfig))
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(rename("keepitsafe.js"))
+            .pipe(gulp.dest("build/app"))
+            .pipe(browserSync.stream());
+});
+
+gulp.task("js", ["js:watchify"]);
 
 /**
  * Generate css
@@ -42,8 +107,8 @@ gulp.task("css", function() {
 /**
  * Generate index.html
  */
-gulp.task("index", ["js", "css"], function() {
-    return gulp.src("index.html")
+gulp.task("html", function() {
+    return gulp.src("./**/*.html")
             .pipe(gulp.dest("build"))
             .pipe(browserSync.stream());
 });
@@ -51,14 +116,16 @@ gulp.task("index", ["js", "css"], function() {
 /**
  * Start server
  */
-gulp.task("start", ["js", "css", "index"], function() {
+gulp.task("start", ["js", "css", "html"], function() {
     browserSync.init({
         server: {
             baseDir: "build/"
         }
     });
 
+    //js.on('update', bundleJs);
+    //b.on('log', gutil.log);
     gulp.watch("app/**/*.ts", ["js"]);
     gulp.watch("css/less/**/*.less", ["css"]);
-    gulp.watch("index.html", ["index"]);//, browserSync.reload]);
+    gulp.watch("index.html", ["html"]);//, browserSync.reload]);
 });
